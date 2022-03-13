7cc9a0dd9a5f78f2f8f5680b503a193fe123c341
Janaki04 <telugujanaki412@gmail.com>
2022-03-13 22:04:31 +0530

    first

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const cors=require("cors")
 
 const router = require("./routes");
 
 const app = express();  
 
-
+app.use(cors());
 app.use("/api/v1", router);
 
 app.listen(8000, ()=>{
